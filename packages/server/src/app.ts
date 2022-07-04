2fd29d6ded6be61dbee27f999f76d643d4ccc368
Kami-Juan <juan1910@live.com.mx>
2022-07-04 15:52:10 -0500

    feat(signup): added mutation

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'app://.'],
+    origin: [process.env.DEV_ENV_URL!, 'app://.'],
     credentials: true,
   }),
 );
--- packages/server/src/infrastructure/trpc/v1.ts
+++ packages/server/src/infrastructure/trpc/v1.ts
@@ -1,12 +1,18 @@
 import superjson from 'superjson';
+import { z } from 'zod';
 import { createRouter } from '../../configuration/context';
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge(
     'auth/',
-    createRouter().mutation('signup', {
-      resolve: async ({ ctx }) => {
+    createRouter().mutation('signup-publisher', {
+      input: z.object({
+        username: z.string().min(6).max(255),
+        password: z.string().min(8).max(255),
+        email: z.string().email(),
+      }),
+      resolve: async ({ input }) => {
         return null;
       },
     }),
